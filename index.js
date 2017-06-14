c2ef5a85e0dc6e562b1745d80910333359f62ca3
Pablo González <pagoru@gmail.com>
2017-06-14 16:06:34 +0200

    1.0.0

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
 /**
  * Created by Pablo on 14 Jun 17.
  */
+const path = require("path");
+const randomSeed = require('random-seed');
+const crypto = require('crypto');
+
+const defaultLetters = "ABCDFGHIJKLMNOPQRSTUVWXYZabdfghijklmnopqrstuvwxyz1234567890";
+
+const hash = (text) => {
+    let shasum = crypto.createHash('sha512');
+    shasum.update(text);
+    return shasum.digest('hex');
+};
+
+module.exports = class SuperSecretSettings {
+    constructor(params){
+        this._masterPassword = params.masterPassword;
+    }
+
+    getMasterPasswordHAsh() {
+        return hash(this._masterPassword);
+    }
+
+    generatePassword(params) {
+        const letters = (params.letters === undefined ? defaultLetters : params.letters).split("");
+        const length = params.length === undefined ? 12 : params.length;
+        const serviceName = params.serviceName;
+
+        let password = "";
+
+        const hexadecimalSeed = hash(this._masterPassword + hash(serviceName));
+        const random = randomSeed.create(hexadecimalSeed);
+
+        for (let i = 0; i < length; i++){
+            let randomLetter = random(letters.length);
+            password += letters[randomLetter];
+        }
+        return password;
+    }
+};
