0cf185b3f810a6775a4e34b7cb5d24d4d6ffaa58
Pablo González <pagoru@gmail.com>
2017-12-17 01:33:28 +0100

    crypto not from core anymore

--- index.js
+++ index.js
@@ -1,36 +1,62 @@
 /**
  * Created by Pablo on 14 Jun 17.
  */
-const path = require("path");
 const randomSeed = require('random-seed');
-const crypto = require('crypto');
+const crypto = require('./crypto.min');
 
-const defaultLetters = "ABCDFGHIJKLMNOPQRSTUVWXYZabdfghijklmnopqrstuvwxyz1234567890";
+const characters = "ABCDFGHIJKLMNOPQRSTUVWXYZabdfghijklmnopqrstuvwxyz1234567890";
 
-const hash = (text) => {
+function getHash(text) {
     let shasum = crypto.createHash('sha512');
     shasum.update(text);
     return shasum.digest('hex');
-};
+}
 
-module.exports = class SuperSecretSettings {
+function getPassword(password, service, length = 12) {
+    let generatedPassword = "";
 
+    const hexadecimalSeed = getHash(password + getHash(service));
+    const random = randomSeed.create(hexadecimalSeed);
+
+    for (let i = 0; i < length; i++)
+        generatedPassword += characters[random(characters.length)];
+
+    return generatedPassword;
+}
+
+module.exports.getHash = getHash;
+module.exports.getPassword = getPassword;
+
+/**
+ * @deprecated since version 2.0.0
+ */
+module.exports.class = class SuperSecretSettings {
+
+    /**
+     * @deprecated since version 2.0.0
+     */
     constructor(params){
         this._masterPassword = params.masterPassword;
     }
 
+    /**
+     * @deprecated since version 2.0.0
+     */
     getMasterPasswordHash() {
-        return hash(this._masterPassword);
+        return getHash(this._masterPassword);
     }
 
+    /**
+     * @deprecated since version 2.0.0
+     */
     generatePassword(params) {
-        const letters = (params.letters === undefined ? defaultLetters : params.letters).split("");
+        const letters = (params.letters === undefined ? characters : params.letters).split("");
         const length = params.length === undefined ? 12 : params.length;
         const serviceName = params.serviceName;
 
         let password = "";
 
-        const hexadecimalSeed = hash(this._masterPassword + hash(serviceName));
+        const hexadecimalSeed = getHash(this._masterPassword + getHash(serviceName));
         const random = randomSeed.create(hexadecimalSeed);
 
         for (let i = 0; i < length; i++){
